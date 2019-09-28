6a78884fcf8e98168fbc340c8fb19fab2c90f319
ithgnm <ithgnm@gmail.com>
2019-09-28 11:12:45 +0700

    news

--- core/views.py
+++ core/views.py
@@ -8,8 +8,8 @@ from news.models import *
 class home_view(View):
     def get(self, request):
         carousel = reversed(Carousel.objects.all().order_by('-pk').reverse()[:3])
-        product = reversed(Product.objects.all().reverse()[:4])
-        category = Category.objects.all()
+        product = Product.objects.filter(active=True).reverse()[:8]
+        category = Category.objects.filter(active=True)
         content = {'carousel': carousel, 'product': product, 'category': category}
         return render(request, 'homepage/index.html', content)
 
